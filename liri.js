b63ed093ca5c4b9de0c455137353f38a06c8584b
johnericward <johnericward@gmail.com>
2019-08-21 13:43:21 -0400

    continued setting up initial files and added some omdb starting code

--- liri.js
+++ liri.js
@@ -17,3 +17,29 @@ spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, d
  
 console.log(data); 
 });
+/*------------------ my stuff after instructional stuff ---------------*/
+
+
+
+/* ------------------- OMDB ------------------ */
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+var axios = require("axios");
+
+
+axios.get(queryUrl).then(
+    function (response) {
+        console.log("------------MOVIE-------------")
+        console.log("movie year is: " + response.data.Year);
+        console.log("---------END MOVIE-------------")
+        // console.log(response.data);
+  })
+
+
+
+console.log(queryUrl);
+
+/* ------------------- END OMDB ------------------ */
+
+/* ------------------- BANDS IN TOWN ------------------ */
+/* ------------------- END BANDS IN TOWN ------------------ */
